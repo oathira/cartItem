d4ca8f9693e8d7b071fdbb5db1dc396af42d176a
athira <athiraos1997@gmail.com>
2023-05-14 19:11:20 +0530

    add increaseQuantity function

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,19 +1,51 @@
 import React from "react";
 
 class CartItem extends React.Component{
+    constructor(){
+        super();
+        this.state =
+        {
+         
+            title:'Mobile phone',
+            price:999,
+            qty:1,
+            img:''
+        }
+      //this.increaseQuantity=this.increaseQuantity.bind(this);
+    }
+    increaseQuantity=()=>{
+        console.log('this',this.state);
+    }
     render()
-    {
+    {  const {title,price,qty}=this.state;
+
         return(
             <div className="cart-item">
                 <div className="left-block">
                     <img style={styles.image}/>
                 </div>
                 <div className="right-block">
-                    <div style={{fontSize:25}}>Phone</div>
-                    <div style={{color:'#777'}}>Rs999</div>
-                    <div style={{color:'#777'}}>Qty:1</div>
+                    <div style={{fontSize:25}}>{title}</div>
+                    <div style={{color:'#777'}}>Rs{price}</div>
+                    <div style={{color:'#777'}}>Qty:{qty}</div>
                     <div className="cart-item-actions">
                         {/*buttons*/}
+                        <img 
+                        alt="increase" 
+                        className="action-icons" 
+                        src="https://cdn-icons-png.flaticon.com/128/992/992651.png"
+                        onClick={this.increaseQuantity}
+                        />
+                        <img 
+                        alt="decrease" 
+                        className="action-icons" 
+                        src="https://cdn-icons-png.flaticon.com/128/992/992683.png"
+                        />
+                        <img 
+                        alt="delete" 
+                        className="action-icons" 
+                        src="https://cdn-icons-png.flaticon.com/128/1214/1214428.png"
+                        />
 
                     </div>
                 </div>
